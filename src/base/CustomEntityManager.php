13693c5e119bf1cc8b88ca8168948076eae1804b
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2017-09-07 23:32:39 +0200

    intro CustomEntityManager

---
+++ src/base/CustomEntityManager.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2017 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\logeia\yii\doctrine\orm;
+
+/**
+ * Description of CustomEntityManager
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class CustomEntityManager extends \abexto\logeia\yii\doctrine\common\base\AbstractDoctrineComponent
+{
+    //put your code here
+}
